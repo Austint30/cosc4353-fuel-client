74de2b5b3ab2ddf5339d12edfcf1d194981f5331
Austin Thibodeaux <austinthibodeaux@gmail.com>
2023-02-12 19:38:57 -0600

    Added example links

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PageContent from 'components/page-content';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <PageContent title='Homepage'>
-            Hello, world!
+            Hello, world!<br/>
+            <Link to="/login">Example link to login page</Link><br/>
+            <Link className='btn btn-primary' to="/login">To login page (Button)</Link>
         </PageContent>
     );
 }
