a467ece6b93e542777a84acd8a9b794d395a3e80
Austin Thibodeaux <austinthibodeaux@gmail.com>
2023-02-13 00:10:24 -0600

    Frontend is deployable again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import GlobalNavbar from './components/global-navbar';
 import Home from './pages/home';
@@ -11,21 +11,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import config from 'config';
 import PageContent from 'components/page-content';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDgToZZsOfj152nmGH8IzhkXup82Ib9BEs",
-    authDomain: "fuelquote-a1262.firebaseapp.com",
-    projectId: "fuelquote-a1262",
-    storageBucket: "fuelquote-a1262.appspot.com",
-    messagingSenderId: "721477043991",
-    appId: "1:721477043991:web:9744543b456f536dde1952",
-    measurementId: "G-9ER20ZPVM9"
-  };
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDgToZZsOfj152nmGH8IzhkXup82Ib9BEs",
+//     authDomain: "fuelquote-a1262.firebaseapp.com",
+//     projectId: "fuelquote-a1262",
+//     storageBucket: "fuelquote-a1262.appspot.com",
+//     messagingSenderId: "721477043991",
+//     appId: "1:721477043991:web:9744543b456f536dde1952",
+//     measurementId: "G-9ER20ZPVM9"
+//   };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
 function App() {
 
