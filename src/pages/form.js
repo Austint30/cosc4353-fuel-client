2208e41e9aebb6f574f8927c9848bcaaff1b708e
SushiAbuse <justine.tran@mavs.uta.edu>
2023-03-14 16:08:00 -0500

    Front-End Fuel Form Match

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,50 +1,84 @@
-import React, { useState } from "react";
+import React from "react";
+import PageContent from 'components/page-content';
+import {Form,Row,Col,Alert,Button} from 'react-bootstrap';
+import useStatefulFetch from 'hooks/stateful-fetch';
+import config from 'config';
 import styles from './forms.module.css';
 
-const Form = () =>  {
-  const [val, setVal] = useState("");
+// Form library we could use: https://react-hook-form.com/
+
+function FuelQuoteForm(){
+
+  const { executeRequest, loading, error, called } = useStatefulFetch(
+    config.serverUrl + '/test', {
+    method: 'get'
+  });
+
+  function submitFuelQuoteForm(){
+    // TODO implement upl   oad Form
+    executeRequest();
+  }
 
   return (
-    <div className="fqf">
-    <h1>Fuel Quote Form</h1>
-    <form className="Fuel_Quote_Form">
-      <div>
-        <label id="request">
-          Gallons Requested: 
-          <input
-            type="number"
-            pattern="[0-9]*"
-            value={val}
-            onChange={(e) =>
-              setVal((v) => (e.target.validity.valid ? e.target.value : v))
-            }
-            placeholder="0"
-            />
-        </label>
-      </div>
-      <div>
-        <label>Delivery Address: </label>
-      </div>
-      <div>
-        <label id="Deliv_Date">
-          Delivery Date: 
-        </label>
-        <input type="date"/>
-      </div>
-      <div>
-        <label id="sug_pric">
-          Suggested Price (per gallon): 
-        </label>
-      </div>
-      <div>
-        <label>
-          Total Amount Due: 
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
-    </div>
+    <>
+    <PageContent title='Fuel Quote Form' />
+    <PageContent>
+    <Form className={styles.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+        submitFuelQuoteForm();
+      }}>
+      <Row>
+          <Col sm={3}>
+            <Form.Group className='mb-3' controlId='reqGallon'>
+              <Form.Label>Gallons Request:</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+          </Col>
+          <Col sm={5}>
+            <Form.Group className='mb-3' controlId='userAddress'>
+              <Form.Label>Delivery Address:</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+          </Col>
+          <Col sm={3}>
+            <Form.Group className='mb-3' controlId='delivDate'>
+              <Form.Label>Delivery Date:</Form.Label>
+              <Form.Control type="date"/>
+            </Form.Group>
+          </Col>
+          </Row>
+          <Row>
+          <Col sm={5}>
+            <Form.Group className='mb-3' controlId='suggPrice'>
+              <Form.Label>Suggested Price/gallon:</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+          </Col>
+          <Col sm={3}>
+            <Form.Group className='mb-3' controlId='total'>
+              <Form.Label>Total:</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+          </Col>
+        </Row>
+      <Button type='submit' variant='primary' disabled={loading}>
+        Save Changes
+      </Button>
+      {called && !error && !loading ? (
+        <Alert className='mt-2' variant='success'>
+          Changes saved successfully! (doesn't acutally save yet...)
+        </Alert>
+      ) : null}
+      {error ? (
+        <Alert className='mt-2' variant='danger'>
+          Something went wrong. We're working on it!
+        </Alert>
+      ) : null}
+    </Form>
+    </PageContent>
+    </>
   );
 }
 
-export default Form;
+export default FuelQuoteForm;
