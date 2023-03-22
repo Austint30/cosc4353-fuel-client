63ee57d772082ea33711e1cec35796c247e3890b
baymax3230 <75866709+danielngo7696@users.noreply.github.com>
2023-03-22 15:11:34 -0500

    Fuel History Table now connected to firebase

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,51 +1,75 @@
 import PageContent from 'components/page-content';
-import { config } from 'config/firebase';
+import React, {useState, useEffect} from "react";
+import {collection} from 'firebase/firestore'
+import { db, config, app } from 'config/firebase';
 import useStatefulFetch from 'hooks/stateful-fetch';
 // import styles from './profile.module.css';
 import { Container, Table } from 'react-bootstrap';
+import {Form,Row,Col,Alert,Button} from 'react-bootstrap';
+import { getAuth } from "firebase/auth";
+import styles from './forms.module.css';
+import {setDoc, getDocs,doc} from "firebase/firestore";
+
 
 // TODO: Add client-side form validation
 // Form library we could use: https://react-hook-form.com/
 
+
 function Test() {
+  const [ loadUserLoading, setLoadUserLoading ] = useState(null);
+  const [ formData, setFormData ] = useState({price: 0}); // added, initializes total to 0 until changes are made
+  const [ userData, setUserData ] = useState(null);
+  const [ loadUserError, setLoadUserError ] = useState(null); // setDoc, Doc
 
-  const { executeRequest, loading, error, called } = useStatefulFetch(
-    config.functionsUrl, {
-    method: 'get'
-  });
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  const [forms, setForms] = useState([]);
+
+  useEffect(()=> {
+    getForms()
+  }, [])
+
+  useEffect(()=>{
+    console.log(forms);
+  }, [forms])
 
 
-  
-  return (
-    <>
+  function getForms(){
+    const formCollectionRef = collection(db, 'form')
+    getDocs(formCollectionRef).then(response => {
+      const frms = response.docs.map(doc => ({data: doc.data(), id:doc.id}))
+      setForms(frms) 
+    }).catch(error => console.log(error.message))
+  }
+
+
+return(
+  <>
+  <div>
     <PageContent title='Fuel History Table' />
     <Container>
     <Table class = "center">
     <thead>
       <tr>
-        <th>Date</th>
-        <th>Price per Gallon</th>
+        <th><ul>Date</ul></th>
+        <th><ul>Price per Gallon</ul></th>
+        <th><ul>Gallons Requested</ul></th>
+        <th><ul>Total</ul></th>
       </tr>
     </thead>
     <tbody>
         <tr>
-            <td>January 1, 2023</td>
-            <td>4.49</td>
-        </tr>
-        <tr>
-            <td>January 2, 2023</td>
-            <td>4.54</td>
-        </tr>
-        <tr>
-            <td>January 3, 2023</td>
-            <td>4.59</td>
+          <td>{forms.map(form => <ul key={form.id}> {form.data.date}</ul>)}</td>
+          <td>{forms.map(form => <ul key={form.id}> {form.data.price}</ul>)}</td>
+          <td>{forms.map(form => <ul key={form.id}> {form.data.gallons}</ul>)}</td>
+          <td>{forms.map(form => <ul key={form.id}> {form.data.total}</ul>)}</td>
         </tr>
     </tbody>
 </Table>
 </Container>
-    </>
-  );
-}
-
-
+  </div>
+  </>
+);
+};
 export default Test;
